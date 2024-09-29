013fd4b55d5a35e9fbc00b3fc9ab137e80265f7c
Damian Gabriel <gabbydamian92@gmail.com>
2024-09-29 11:17:53 +0100

    production wan kill me

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   // Check if running in production or development
   return process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_PRODUCTION_URL; // Set this in your .env file for production
+    : "https://obash-signatures.vercel.app"; 
 };
 
 const baseUrl = getBaseUrl();
