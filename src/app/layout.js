1001019b1b9e1789fd2f7adee137710a6e05749f
Damian Gabriel <gabbydamian92@gmail.com>
2024-10-22 10:57:07 +0100

    added favicon and robots

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
+        {/* <link
           rel="icon"
           href="/favicon.ico"
           type="image/x-icon"
           sizes="16x16"
+        ></link> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
         ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
         <script
           src="https://widget.cloudinary.com/v2.0/global/all.js"
           type="text/javascript"
